6598d34bc07ba12e9501af3ea842be9ab849734c
prpaul <prpaul@lexmark.com>
2019-09-27 18:49:21 +0530

    Services to retrieve data from google api and iTunes api

---
+++ src/main/java/com/my/company/media/service/MediaAggregatorService.java
@@ -0,0 +1,93 @@
+package com.my.company.media.service;
+
+import com.my.company.media.exception.BookApiCommunicationException;
+import com.my.company.media.exception.ITunesApiCommunicationException;
+import com.my.company.media.exception.NoMediaFoundException;
+import com.my.company.media.model.dto.Album;
+import com.my.company.media.model.dto.Book;
+import com.my.company.media.model.dto.Media;
+import com.my.company.media.model.dto.google.book.BookApiResponse;
+import com.my.company.media.model.dto.google.book.VolumeInfo;
+import com.my.company.media.model.dto.itunes.album.ITunesApiResponse;
+import com.my.company.media.service.album.AlbumRetrivalService;
+import com.my.company.media.service.book.BookRetrivalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by prpaul on 9/26/2019.
+ */
+@Service
+public class MediaAggregatorService {
+    private final Logger logger = LoggerFactory.getLogger(MediaAggregatorService.class);
+    @Autowired
+    private BookRetrivalService bookService;
+
+    @Autowired
+    private AlbumRetrivalService albumService;
+
+    @Cacheable(cacheNames="mediaCache", key="#searchKey")
+    public List<Media> searchMedia(final String searchKey) throws NoMediaFoundException{
+        List<Media> mediaInformations = new LinkedList<Media>();
+        boolean bookRetrivalSuccessful = populateBooks(mediaInformations, searchKey);
+        boolean albumRetrivalSuccessful = populateAlbums(mediaInformations, searchKey);
+        if(bookRetrivalSuccessful || albumRetrivalSuccessful){
+            List<Media> result = mediaInformations.stream().sorted(Comparator.comparing(Media::getTitle)).collect(Collectors.<Media>toList());
+            return result;
+        }
+        String errorMsg = "Unable to retrieve informations from external apis with search key :"+searchKey;
+        logger.error(errorMsg);
+        throw new NoMediaFoundException(errorMsg);
+    }
+
+    private boolean populateAlbums(List<Media> mediaInformations, String searchKey){
+        try{
+            ITunesApiResponse response = albumService.retrieveAlbum(searchKey);
+            mediaInformations.addAll(convertItunesResponse(response));
+            return true;
+        }catch(ITunesApiCommunicationException e){
+            logger.error("Unable to retrieve albums from itunes music api with search key :"+searchKey ,e);
+            return false;
+        }
+    }
+
+    private boolean populateBooks(List<Media> mediaInformations, String searchKey){
+        try{
+            BookApiResponse bookApiResponse = bookService.retrieveBook(searchKey);
+            mediaInformations.addAll(convertGoogleBookResponse(bookApiResponse));
+            return true;
+        }catch(BookApiCommunicationException e){
+            logger.error("Unable to retrieve books from google book api with search key :"+searchKey ,e);
+            return false;
+        }
+    }
+
+    private List<Book> convertGoogleBookResponse(BookApiResponse bookApiResponse){
+        List<Book> books = new ArrayList<Book>();
+        bookApiResponse.getItems().forEach(it -> {
+            Optional<VolumeInfo> volumnInfo = Optional.ofNullable(it.getVolumeInfo());
+            volumnInfo.ifPresent(vol -> {
+                Book book = new Book(vol.getAuthors(), vol.getTitle());
+                books.add(book);
+            });
+
+        });
+        return books;
+    }
+
+    private List<Album> convertItunesResponse(ITunesApiResponse response){
+        List<Album> albums = new ArrayList<Album>();
+        response.getResults().forEach(it ->{
+            Album album = new Album(it.getArtist(), it.getCollectionName());
+            albums.add(album);
+        });
+        return albums;
+    }
+
+}
---
+++ src/main/java/com/my/company/media/service/book/BookRetrivalService.java
@@ -0,0 +1,48 @@
+package com.my.company.media.service.book;
+
+import com.my.company.media.aop.TrackTime;
+import com.my.company.media.configuration.AppProperties;
+import com.my.company.media.exception.BookApiCommunicationException;
+import com.my.company.media.model.dto.google.book.BookApiResponse;
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.MessageFormat;
+
+/**
+ * Created by prpaul on 9/26/2019.
+ */
+@Service
+public class BookRetrivalService {
+    private final Logger logger = LoggerFactory.getLogger(BookRetrivalService.class);
+    @Autowired
+    private AppProperties config;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @TrackTime
+    public BookApiResponse retrieveBook(final String searchKey) throws BookApiCommunicationException {
+        String url = MessageFormat.format(config.getGoogleBookApi(), bookRetrivalParams(searchKey));
+        try{
+            BookApiResponse response = restTemplate.getForObject(url, BookApiResponse.class);
+            return response;
+        }catch(HttpClientErrorException e){
+            String errorMsg = "Unable to retrieve book informations from Google Book Api.";
+            logger.error(errorMsg, e);
+            throw new BookApiCommunicationException(errorMsg, e);
+        }catch(Exception e){
+            String errorMsg = "Communication error with Google Book Api.";
+            logger.error(errorMsg, e);
+            throw new BookApiCommunicationException(errorMsg, e);
+        }
+    }
+
+    private Object[] bookRetrivalParams(final String searchKey){
+        return new Object[]{searchKey, "english", config.getResultSize(), "relevance"};
+    }
+}
